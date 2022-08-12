9b2383969e53691fa347706d7c33661482db5a23
Peixoto07 <lucaspeixotodacruz@gmail.com>
2022-08-12 02:20:42 -0300

    Modificações finais

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
-# Projeto-Cadastro-de-Produtos
-### Pequeno projeto focado na prática do JavaScript puro.
-Front-End básico feito com html e css, porém foco principal é o uso do javascript, responsável pela interação do usuário, realizando ações de **adicionar**, **editar** e **excluir** os itens
+# Cadastro-de-Produtos_(CRUD)
+### Pequeno projeto de C.R.U.D focado na prática do JavaScript puro.
+
+Objetivo: Aperfeiçoar a prática das principais tarefas de qualquer sistema , *Criar, Ler, Atualizar e Excluir*
+
+Front-End básico feito com **HTML5** e **CSS** puro com algumas animações **CSS** utilizando troca de classes com **JavaScript**, porém o foco principal é o uso do JavaScript como linguagem principal do projeto, responsável por toda logística de ler os dados, armazenar no **LocalStorage** e imprimir os dados no DOM.
+
+
+
--- assets/main.js
+++ assets/main.js
@@ -1,60 +1,106 @@
 function criaProduto() {
     const getArrayProduto = () => JSON.parse(localStorage.getItem('meu_array'))
-    
+    const setArrayProduto = (arrayDB) => localStorage.setItem('meu_array', JSON.stringify(arrayDB))
+    const card = document.getElementById('card')
+    const corpoTabela = document.getElementById('corpoTabela')
+    const botaoSalvar = document.getElementById('salvar')
+    const botaoLimpar = document.getElementById('limpar')
+    const valueNome = document.getElementById('nome')
+    const valuePreco = document.getElementById('preco')
+    function removeOuAddModal(metodo, card, botaoSalvar, botaoLimpar) {
+        if (metodo == 'remove') {
+            card.classList.remove('modalCard') == 'removeCard';
+            botaoSalvar.classList.remove('atualizar') == 'removeSalvar';
+            botaoLimpar.classList.remove('cancelar') == 'removeLimpar';
+            return
+            'removeCard', 'removeSalvar', 'removeLimpar'
+        }
+
+        card.classList.add('modalCard') == 'addCard',
+            botaoSalvar.classList.add('atualizar') == 'addSalvar',
+            botaoLimpar.classList.add('cancelar') == 'addLimpar'
+        return
+        'addCard,addSalvar,removeLimpar'
+    }
+
+    const vereficaDB = () => {
+        if (localStorage.meu_array == 'null') {
+            localStorage.meu_array = []
+        }
+        return vereficaDB()
+    }
+
+    function newID() {
+        if (localStorage.meu_array && localStorage.meu_array != '[]' && localStorage.meu_array != 'null') {
+            id = getArrayProduto()[getArrayProduto().length - 1].id + 1
+            return id;
+        }
+
+        return id = 1;
+    }
+
+    // *** INICIO APP *** //
+
     const novoProduto = {
-        
-        id: 1,
+        id: newID(),
+        editId: null,
         arrayProdutos: [],
-        
+
 
         salvar() {
-
             const produto = novoProduto.lerDados();
+            // newID()
+            console.log(novoProduto.editId);
+
             if (novoProduto.validaCampos(produto)) {
-                // console.log(novoProduto.arrayProdutos);
-                novoProduto.adicionaNoDB(produto)
+                if (novoProduto.editId == null) {
+                    novoProduto.adicionaNoDB(produto)
+                } else {
+                    novoProduto.botaoAtualizar(novoProduto.editId)
+                }
                 novoProduto.limpaTbody()
-                
+                novoProduto.atualizaTabela()
+
             }
-            // novoProduto.criaLinha()
-            novoProduto.atualizaTabela()
-            novoProduto.limpar()
+
+            novoProduto.limparCampos()
         },
 
-        criaLinha(id,nome,preco ){
+        criaLinha(id, nome, preco) {
             const novaLinha = document.createElement('tr')
             novaLinha.innerHTML = `
                 <td class="center">${id}</td>
                 <td>${nome}</td>
                 <td>R$ ${preco}</td>
                 <td class="center">
-                    <img src="img/editar.png" alt="">
-                    <img src="img/deletar.png" alt="">
+                    <input class = "buttonAcoes" onclick="appCadastro.editar(${id})" type="image" src="img/editar.png"/>
+                    <input class = "buttonAcoes" onclick="appCadastro.deletar(${id})" type="image" src="img/deletar.png"/>
+                    
                 </td>
             `
-            document.getElementById('corpoTabela').appendChild(novaLinha)
-            
+            corpoTabela.appendChild(novaLinha)
+
         },
-        
+
         atualizaTabela() {
             // novoProduto.limpaTbody()
-            getArrayProduto().forEach(novaLinha => 
-                novoProduto.criaLinha(novaLinha.id,novaLinha.nome,novaLinha.preco))
-                return
+            if (localStorage.meu_array) {
+                getArrayProduto().forEach(novaLinha =>
+                    novoProduto.criaLinha(novaLinha.id, novaLinha.nome, novaLinha.preco))
+            }
 
         },
 
 
         adicionaNoDB(produto) {
+            produto.preco= parseFloat(produto.preco).toFixed(2)
             const arrayDB = getArrayProduto() ?? []
             arrayDB.push(produto)
             novoProduto.id++
-            // localStorage.meu_array = JSON.stringify(novoProduto.arrayProdutos)
-            localStorage.setItem('meu_array', JSON.stringify(arrayDB))
-        
-            
+            setArrayProduto(arrayDB)
+
         },
-        
+
 
 
         lerDados() {
@@ -62,8 +108,8 @@ function criaProduto() {
             const produto = {}
 
             produto.id = novoProduto.id;
-            produto.nome = document.getElementById('nome').value;
-            produto.preco = document.getElementById('preco').value;
+            produto.nome = valueNome.value;
+            produto.preco = valuePreco.value;
 
             return produto
         },
@@ -76,25 +122,75 @@ function criaProduto() {
             return true
         },
 
-        limpaTbody (){
-            const corpoTabela =document.getElementById('corpoTabela')
-            while(corpoTabela.firstChild){
+        limpaTbody() {
+
+            while (corpoTabela.firstChild) {
                 corpoTabela.removeChild(corpoTabela.lastChild)
             }
         },
 
-        limpar() {
-            document.getElementById('nome').value = ''
-            document.getElementById('preco').value = ''
+        limparCampos() {
+            valueNome.value = ''
+            valuePreco.value = ''
+            if (card.className == "card modalCard") {
+                removeOuAddModal('remove', card, botaoSalvar, botaoLimpar)
+                botaoSalvar.innerText = 'Salvar'
+                botaoLimpar.innerText = 'Limpar'
+                novoProduto.editId = null
+            }
+        },
 
-        }
+        botaoAtualizar(id) {
+            const arrayDB = getArrayProduto()
+
+            for (let i = 0; i < arrayDB.length; i++) {
+                if (arrayDB[i].id == id) {
+                    arrayDB[i].nome = valueNome.value
+                    arrayDB[i].preco = parseFloat(valuePreco.value).toFixed(2)
+                    setArrayProduto(arrayDB)
+                }
+            }
+        },
+
+        editar(id, i) {
+            const arrayDB = getArrayProduto()
+
+            for (let i = 0; i < arrayDB.length; i++) {
+                if (arrayDB[i].id == id) {
+                    novoProduto.editId = id
+                    removeOuAddModal('add', card, botaoSalvar, botaoLimpar)
+                    valueNome.value = arrayDB[i].nome
+                    valuePreco.value = arrayDB[i].preco
+                    botaoSalvar.innerText = 'Atualizar'
+                    botaoLimpar.innerText = 'Cancelar'
+                    console.log(id);
+                }
+            }
+        },
+
+
+        deletar(id, i) {
+            const arrayDB = getArrayProduto()
+
+            for (let i = 0; i < arrayDB.length; i++) {
+                if (arrayDB[i].id == id) {
+                    arrayDB.splice(i, 1)
+                    corpoTabela.deleteRow(i)
+                    setArrayProduto(arrayDB)
+                    arrayDB.length < 1 ? location.reload() : {}
+
+                }
+            }
+
+        },
     }
-    
+
     atualizaTabela = novoProduto.atualizaTabela
     return novoProduto
-    
+
 }
 
 const appCadastro = criaProduto()
-atualizaTabela()
+appCadastro.atualizaTabela()
+
 
